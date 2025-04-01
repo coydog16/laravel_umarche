7f6bfeaef3bdc892b586eace21526fea07bedc69
coydog16 <coydog16@gmail.com>
2025-04-01 19:20:47 +0900

    変数で設定

--- resources/views/components/tests/card.blade.php
+++ resources/views/components/tests/card.blade.php
@@ -3,4 +3,5 @@
         <h2 class="text-xl font-bold mb-2">{{ $title }}</h2>
         <div>画像</div>
         <p class="text-gray-700 mb-4 fontsize-4">{{ $description }}</p>
+        <div>{{ $message }}</div>
 </div>
