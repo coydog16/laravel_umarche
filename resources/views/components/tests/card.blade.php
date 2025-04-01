02b3b8a217dc71ab1154cee3b5ea7d6d3d23647b
coydog16 <coydog16@gmail.com>
2025-04-01 19:08:25 +0900

    属性で設定

---
+++ resources/views/components/tests/card.blade.php
@@ -0,0 +1,6 @@
+<div class="border02 shadow-md w-1/4 p-2">
+    <div class="flex flex-col items-center">
+        <h2 class="text-xl font-bold mb-2">{{ $title }}</h2>
+        <div>画像</div>
+        <p class="text-gray-700 mb-4 fontsize-4">{{ $description }}</p>
+</div>
--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,3 +1,4 @@
 <x-tests.app>
 コンポーネントテスト１
+<x-tests.card title="タイトル" description="説明" />
 </x-tests.app>
