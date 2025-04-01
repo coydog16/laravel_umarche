ab7407ba37b28266e3c705e8e0bb61b18827d35a
coydog16 <coydog16@gmail.com>
2025-04-01 19:32:52 +0900

    props

--- resources/views/components/tests/card.blade.php
+++ resources/views/components/tests/card.blade.php
@@ -1,7 +1,14 @@
+@props([
+  'title' => 'タイトル初期値です。',
+  'message' => '初期値です。',
+  'description' => '説明初期値です。'
+  ])
+
 <div class="border02 shadow-md w-1/4 p-2">
     <div class="flex flex-col items-center">
         <h2 class="text-xl font-bold mb-2">{{ $title }}</h2>
         <div>画像</div>
         <p class="text-gray-700 mb-4 fontsize-4">{{ $description }}</p>
         <div>{{ $message }}</div>
+    </div>
 </div>
--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -1,4 +1,7 @@
+
+
 <x-tests.app>
-コンポーネントテスト１
-<x-tests.card title="タイトル" description="説明" :message="$message" />
+    コンポーネントテスト１
+    <x-tests.card title="タイトル" description="説明" :message="$message" />
+    <x-tests.card title="タイトル" />
 </x-tests.app>
