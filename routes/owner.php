841adba5bcb55b88a4372e820251837ff032ef9f
coydog16 <coydog16@gmail.com>
2025-04-05 05:20:00 +0900

    2025/4/5リダイレクトバグ修正

--- routes/owner.php
+++ routes/owner.php
@@ -11,7 +11,7 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['auth:owners', 'verified'])->name('dashboard');
 
 Route::middleware('auth:owners')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
