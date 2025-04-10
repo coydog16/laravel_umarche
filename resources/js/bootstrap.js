00c46a3f0d47e2c578c65e1ccb6ab4d6a3834c8a
coydog16 <coydog16@gmail.com>
2025-04-11 01:19:41 +0900

    micromodal

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,3 +2,8 @@ import axios from 'axios';
 window.axios = axios;
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+import MicroModal from 'micromodal';  // es6 module
+MicroModal.init({
+  disableScroll: true
+});
